9cac1e3ebcae499e8b9d066c8497a1517babc81f
Tobi Albertino <muhamad_tobiaja232@apps.ipb.ac.id>
2022-11-25 00:21:43 +0700

    add not null

--- migrations/posgres/20221116090920_create_table_to_do.up.sql
+++ migrations/posgres/20221116090920_create_table_to_do.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE to_do (
   id BIGSERIAL NOT NULL,
   title TEXT NOT NULL,
   status BOOLEAN NOT NULL,
-  created_at BIGINT,
-  updated_at BIGINT,
+  created_at BIGINT NOT NULL,
+  updated_at BIGINT NOT NULL,
 
   PRIMARY KEY (id)
 );
